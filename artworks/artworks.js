ff2ae1197653e15a96109488561c717af7c646ba
idaboren <idaboren@gmail.com>
2025-12-29 13:07:44 +0100

    gallery layout

---
+++ artworks/artworks.js
@@ -0,0 +1,132 @@
+const main = document.getElementsByTagName("main")[0];
+let path = "../artworks/"
+let tags = [];
+let artData = [];
+
+await setArtData(); // set artdata from artworks.json
+
+export function createSectionsForEveryTag() {
+    for (let i = 0; i < tags.length; i++){
+        if (tags[i].name != "frontpage")
+            createSectionByTag(tags[i])
+    }
+}
+
+export function createSectionForFrontpage() {
+    let frontpage = tags.find(t => t.name == "frontpage")
+    createSectionByTag(frontpage);
+}
+
+function createSectionByTag(tag) {
+    const section = document.createElement("section");
+    const sectionContent = document.createElement("div");
+    const column1 = document.createElement("div");
+    const column2 = document.createElement("div");
+    const column3 = document.createElement("div");
+    const column4 = document.createElement("div");
+    const column5 = document.createElement("div");
+    const column6 = document.createElement("div");
+    const sectionTitle = document.createElement("h3");
+    const sectionDescription = document.createElement("p");
+    let sectionLength = 0;
+    let sectionArtList = [];
+
+    sectionTitle.innerText = tag.name;
+    sectionDescription.innerText = tag.description;
+    sectionDescription.classList.add("section-description");
+    sectionContent.classList.add("section-content");
+    column1.classList.add("column-1");
+    column2.classList.add("column-2");
+    column3.classList.add("column-2");
+    column4.classList.add("column-3");
+    column5.classList.add("column-3");
+    column6.classList.add("column-3");
+
+    for (let i = 0; i < artData.length; i++) {
+        if ((artData[i].tags).includes(tag.name)) {
+            sectionLength ++;
+            sectionArtList.push(artData[i]);
+        }
+    }
+
+    for (let i = 0; i < sectionArtList.length; i++){
+        const figure = document.createElement("figure");
+        const figureImg = document.createElement("img");
+        // const figureCaption = document.createElement("figureCaption");
+
+        figure.classList.add("artwork-item");
+        figureImg.src = path + sectionArtList[i].path;
+        figureImg.alt = sectionArtList[i].altText;
+        figureImg.classList.add("artwork-image");
+        // figureCaption.innerText = sectionArtList[i].title;
+        // figureCaption.classList.add("artwork-title");
+
+        figure.append(figureImg);
+        if (sectionLength == 1) {
+            column1.append(figure);
+        } 
+        else if (sectionLength == 2) {
+            if ( i == 0 )
+                column2.append(figure);
+            else
+                column3.append(figure);
+        }
+        else {
+            if (i<sectionLength/3) {
+                column4.append(figure);
+            }
+            else if (i>(sectionLength/3) && i<=((sectionLength/3)*2)) {
+                column5.append(figure);
+            }
+            else {
+                column6.append(figure);
+            }
+        }
+
+    }
+
+    main.append(section);
+
+    if (sectionLength >= 3) {
+        if (sectionTitle.innerText != "frontpage") { 
+            section.append(sectionTitle);
+            sectionDescription.innerText ? section.append(sectionDescription) : null; 
+        }
+        section.append(sectionContent);
+        sectionContent.append(column4, column5, column6);
+    }
+    else if (sectionLength == 2) {
+        section.append(sectionTitle);
+        sectionDescription.innerText ? section.append(sectionDescription) : null; 
+
+        section.append(sectionContent);
+        sectionContent.append(column2, column3);
+    }
+    else {
+        column2.append(sectionTitle);
+        sectionDescription.innerText ? column2.append(sectionDescription) : null; 
+        section.append(sectionContent);
+
+        sectionContent.append(column1, column2);
+    }
+}
+
+async function setArtData() {
+  const path = "../artworks/artworks.json";
+  await fetch(path)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Images could not load: ' + response.statusText);
+            //backup solution?
+        }
+        return response.json();
+    })
+    .then(data => {
+        artData = data.artworks;
+        tags = data.tags;
+    })
+    .catch(error => {
+        console.error('Images could not load: ', error);
+        //backup solution?
+    });
+}
---
+++ header/header.js
@@ -0,0 +1,14 @@
+const header = document.getElementsByTagName("header")[0];
+header.innerHTML = 
+'<h1>' +
+    '<a href=../index.html>Ida Borén</a>' +
+'</h1>' +
+// '<h2>' +
+//     '<a href="../pages/contact.html">... is open for commissions!</a>' +
+// '</h2>' +
+'<nav>' +
+    '<ul id="nav-list">' +
+        '<a href="../pages/portfolio.html">Portfolio</a>' +
+        '<a href="../pages/contact.html">Contact</a>' +
+    '</ul>' +
+'</nav>';
